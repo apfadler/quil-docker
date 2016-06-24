e761c37d6e3d63ee76fbcdca7fe1fcbb421ec094
apfadler <andreas.pfadler@gmail.com>
2016-06-24 10:29:55 +0200

    [POC-4] zeppelin ignite config from xmls

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -25,6 +25,7 @@ chmod a+x ../../quantlib-quil-aws-worker/dist/bin/*.sh
 
 mkdir ../../quantlib-zeppelin/libs
 cp -r dist/libs/* ../../quantlib-zeppelin/libs
+cp dist/config/*.xml ../quantlib-zeppelin/
 
 
 cd ../../bin
