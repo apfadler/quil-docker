f9c2ebe01b78c69bbe50059e5cc8270c0c392fb0
apfadler <andreas.pfadler@gmail.com>
2017-01-04 14:01:13 +0100

    [POC-4] No cache for zeppelin image build

--- bin/build-zeppelin-docker-image.sh
+++ bin/build-zeppelin-docker-image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd $QUIL_HOME/quantlib-zeppelin
-docker build -t zeppelin-test .
+docker build --no-cache -t zeppelin-test .
 docker tag -f zeppelin-test apfadler/quantlib-zeppelin
 docker push apfadler/quantlib-zeppelin
